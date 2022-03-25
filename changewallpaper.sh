aadf6393c3b3e0d42956f67770d6daa8567a6838
Vincchy <>
2022-03-25 18:02:48 +0100

    Update

--- changewallpaper.sh
+++ changewallpaper.sh
@@ -1,6 +1,7 @@
 # !/bin/sh
 
 # Generate colors with pywal and recompile dwm
+
 . $HOME/.local/bin/setwal.sh
 
 # Set the apropriate colors in dunst config
@@ -9,5 +10,19 @@ $HOME/.local/bin/setdunst.sh > $HOME/.config/dunst/dunstrc
 pkill dunst
 dunst &
 
+. $HOME/.cache/wal/colors.sh
+
+pywalfox update
+pywal-discord $HOME/.cache/wal/colors.css
+
+# Theme for rofi
+cat $HOME/.cache/wal/colors-rofi-light.rasi > $HOME/.config/rofi/config.rasi
+
+# color0 - dark background
+# color2 - active workspace + bar
+# color7 - active
+# color8 - not active
+curl -X POST -d "color0=${color0}&color1=${color2}&color2=${color7}&color3=${color8}" --silent -o /dev/null 192.168.0.222:5000/colors
+
 # Restart dwm
-echo "You can now restart dwm ( MOD+Shift+M )"
+echo "You can now restart dwm ( MOD+Shift+M )" > /dev/null
--- setwal.sh
+++ setwal.sh
@@ -1,23 +1,51 @@
 # !/bin/sh
 
-# Choose a random .jpg wallpaper from $HOME/pic/wal
-wall=$(find $HOME/pic/wal -type f | shuf -n 1)
+# VARIABLES ---
+# dmenu - path to dmenu
+# root_dir - path to wallpapers
+dmenu=$HOME/.local/src/dmenu/dmenu
+root_dir=$HOME/pic/wal
 
-# Set the wallpaper
-xwallpaper --zoom $wall
 
-# Generate the colors
-wal -i $wall
+# Choose a directory from which to choose the wallpapers
+current_dir=$(basename -a $(find $root_dir -mindepth 1 -type d) | $dmenu)
 
+# When the directory hasn't been chosen exit
+if [ -z "$current_dir" ]; then
+	exit 1
+fi
+
+# Choose a wallpaper (random for random wallpaper)
+wall=$root_dir/$current_dir/$(echo -e "random\n$(basename -a $(find $root_dir/$current_dir -type f))" | $dmenu)
+if [ "$wall" == "$root_dir/$current_dir/random" ]; then
+	wall=$(find $root_dir/$current_dir -type f | shuf -n 1)
+fi
+
+# When the wallpaper hasn't been chosen exit
+if [ -z "$wall" ]; then
+	exit 1
+fi
+
+# Generate the colors (skip changing the theme for tty and terminals [-st] same for changing the wallpaper [-n])
+wal -i $wall -st -n -q
+
+# Cut a section from dwm color config (otherwise not working for some reason)
 sed -i 'N;$!P;D' $HOME/.cache/wal/colors-wal-dwm.h
 
+# Theme for dwm
 cd $HOME/.local/src/dwm/
 cat config.template.h > config.h
-
-#infile=$HOME/.cache/wal/colors-wal-dwm.h
-#outfile=$HOME/.local/src/dwm/config.h
 sed -i -e '/PLACE-COLORS-HERE/r /home/vinchy/.cache/wal/colors-wal-dwm.h' /home/vinchy/.local/src/dwm/config.h
+make > /dev/null
 
-make
+# Theme for dmenu
+cd $HOME/.local/src/dmenu/
+cat config.template.h > config.h
+sed -i -e '/PLACE-COLORS-HERE/r /home/vinchy/.cache/wal/colors-wal-dmenu.h' /home/vinchy/.local/src/dmenu/config.h
+make > /dev/null
 
+# Restart dwm
 pkill dwm
+
+# Set the wallpaper
+wal -i $wall -q
